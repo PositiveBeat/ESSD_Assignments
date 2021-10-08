170ae76effb450b86fb0aecb918644596aee782e
nthom18 <nthom18@student.sdu.dk>
2021-10-08 20:55:23 +0200

    Fixed uart constant repeater

--- As1 Modules/constant_repeater.vhd
+++ As1 Modules/constant_repeater.vhd
@@ -12,37 +12,25 @@ end constant_repeater;
 
 
 architecture Behavioral of constant_repeater is
-    signal const        :std_logic_vector(7 downto 0) := "01000001";    -- ASCII 'A'
-    signal counter      :unsigned(3 downto 0) := (others => '0');
+    signal const        :std_logic_vector(7 downto 0) := "00110100";    -- ASCII '4'
     signal send_flag    :std_logic := '0';
-    signal send    :std_logic := '0';
 begin
 
     process(clk, en)
     begin
-    
-        if (en = '1' AND send_flag = '0') then
-            send_flag <= '1';
---            counter <= (others => '0');
-        end if;
-    
-        if (rising_edge(clk) AND send_flag = '1' AND en = '0') then
---            counter <= counter + 1;
+
+        if (rising_edge(clk)) then
+        
+            if (en = '1') then
+                intr <= '1';
+            else
+                intr <= '0';
+            end if;
             
---            if (counter = 15) then
---                send <= '1';
---            else
---                send <= '0';
---            end if;
-
-            intr <= send_flag;
-
-            send_flag <= '0';
         end if;
     
     end process;
 
---intr <= send;
 data_out <= const;
 
 end Behavioral;
